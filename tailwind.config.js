6d25039559cd2ae8ab17c997d23f4cc64198b48a
fine-e-s <fineevgeny@gmail.com>
2023-03-10 02:49:55 +0400

    Add 'Space Mono' font to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      mono : ['"Space Mono"', 'monospace']
+    },
     extend: {},
   },
   plugins: [],
