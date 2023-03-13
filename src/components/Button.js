557386796ff179f71aaa2daaf3c36925d07ef82a
fine-e-s <fineevgeny@gmail.com>
2023-03-13 13:26:27 +0400

    Change positioning of underline block to abosulte

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,24 +7,20 @@ export default function Button({
 }) {
   return (
     <div
-      className={`cursor-pointer px-5 py-3 ${
+      className={`mx-5 cursor-pointer py-3 ${
         cta
-          ? "flex items-center gap-3 rounded-[20px] bg-purple-500 px-7 py-5"
+          ? "!mx-0 flex items-center gap-3 rounded-[20px] bg-purple-500 px-7 py-5"
           : ""
       } ${
         large
           ? "flex w-[224px] justify-center gap-3 rounded-[20px] bg-purple-500 px-5 py-12 font-semibold"
           : ""
-      } ${hoverScale ? "hoverScale" : ""}  ${
-        hoverUnderline ? "hoverUnderline group overflow-hidden" : ""
-      }`}
+      }      
+      ${hoverScale ? "hoverScale" : ""}
+      ${hoverUnderline ? "group relative" : ""}`}
     >
       {children}
-      {hoverUnderline ? (
-        <div className="block h-[4px] w-full -translate-x-1/2 scale-x-0 bg-purple-500 transition duration-150 ease-in-out group-hover:translate-x-0 group-hover:scale-x-100"></div>
-      ) : (
-        ""
-      )}
+      {hoverUnderline ? <div className="underline"></div> : ""}
     </div>
   );
 }
