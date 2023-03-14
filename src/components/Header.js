942deefc15a718367efaaff683b0046444119d61
fine-e-s <fineevgeny@gmail.com>
2023-03-14 16:28:23 +0400

    Make header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,22 @@ import Button from "./Button";
 export default function Header() {
   return (
     <>
-      <div className="flex justify-center px-[50px]">
-        <div className="flex h-24 w-full items-center">
-          <div className="translate-y-[-2px]">
-            <Link href="/">
-              <img src="icons/logo.svg" className="hoverScale" />
-            </Link>
-          </div>
-          <div className="ml-auto flex items-center gap-2.5 font-semibold">
-            <Link href="/marketplace">
-              <Button hoverUnderline>Marketplace</Button>
-            </Link>
-            <Button hoverUnderline>Ranking</Button>
-            <Button hoverUnderline>Connect a wallet</Button>
-            <Button cta hoverScale>
-              <img src="icons/user.svg" style={{ background: "transparent" }} />
-              Sign Up
-            </Button>
-          </div>
+      <div className="my-4 flex h-auto w-full flex-col items-start gap-[16px] px-[50px] lg:h-24 lg:flex-row lg:items-center">
+        <div className="translate-y-[-2px]">
+          <Link href="/">
+            <img src="icons/logo.svg" className="hoverScale" />
+          </Link>
+        </div>
+        <div className="ml-0 flex flex-col items-start gap-2.5 font-semibold lg:ml-auto lg:flex-row lg:items-center">
+          <Link href="/marketplace">
+            <Button hoverUnderline>Marketplace</Button>
+          </Link>
+          <Button hoverUnderline>Ranking</Button>
+          <Button hoverUnderline>Connect a wallet</Button>
+          <Button cta hoverScale>
+            <img src="icons/user.svg" style={{ background: "transparent" }} />
+            Sign Up
+          </Button>
         </div>
       </div>
     </>
