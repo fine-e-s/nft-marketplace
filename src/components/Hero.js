0fceca2aba5a6857fac01a445033afc64495ebfd
fine-e-s <fineevgeny@gmail.com>
2023-03-14 17:36:19 +0400

    Make hero section adaptive to medium screens

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,12 @@ export default function Hero() {
   return (
     <div className="flex h-[700px] items-center justify-center">
       <div className="flex justify-center gap-[30px]">
-        <div className="flex h-[544px] w-[510px] flex-col gap-[30px]">
+        <div className="flex h-[544px] flex-col gap-[30px] md:w-[300px] lg:w-[510px]">
           <div className="flex flex-col gap-[20px]">
-            <div className="text-[67px] font-semibold leading-[1.1]">
+            <div className="font-semibold leading-[1.1] md:text-[38px] lg:text-[67px]">
               Discover Digital Art &amp; Collect NFTs
             </div>
-            <div className="text-[22px] leading-[1.6]">
+            <div className="leading-[1.6] md:text-[16px] lg:text-[22px]">
               NFT marketplace UI created with Anima for Figma. Collect, buy and
               sell art from more than 20k NFT artists.
             </div>
@@ -21,34 +21,34 @@ export default function Hero() {
           </Button>
 
           <div className="flex flex-row gap-[30px]">
-            <div className="flex w-[150px] flex-col justify-start">
-              <div className="font-mono text-[28px] font-bold leading-[1.4]">
+            <div className="flex flex-col justify-start md:w-[90px] lg:w-[150px]">
+              <div className="font-mono font-bold leading-[1.4] md:text-[22px] lg:text-[28px]">
                 240k+
               </div>
-              <div className="text-[24px] font-normal leading-[1.6]">
+              <div className="font-normal leading-[1.6] md:text-[16px] lg:text-[24px]">
                 Total Sale
               </div>
             </div>
-            <div className="flex w-[150px] flex-col justify-start">
-              <div className="font-mono text-[28px] font-bold leading-[1.4]">
+            <div className="flex flex-col justify-start md:w-[90px] lg:w-[150px]">
+              <div className="font-mono font-bold leading-[1.4] md:text-[22px] lg:text-[28px]">
                 100k+
               </div>
-              <div className="text-[24px] font-normal leading-[1.6]">
+              <div className="font-normal leading-[1.6] md:text-[16px] lg:text-[24px]">
                 Auctions
               </div>
             </div>
-            <div className="flex w-[150px] flex-col justify-start">
-              <div className="font-mono text-[28px] font-bold leading-[1.4]">
+            <div className="flex flex-col justify-start md:w-[90px] lg:w-[150px]">
+              <div className="font-mono font-bold leading-[1.4] md:text-[22px] lg:text-[28px]">
                 240k+
               </div>
-              <div className="text-[24px] font-normal leading-[1.6]">
+              <div className="font-normal leading-[1.6] md:text-[16px] lg:text-[24px]">
                 Artists
               </div>
             </div>
           </div>
         </div>
-        <div className="flex h-[510px] w-[510px] flex-col overflow-hidden rounded-[20px] bg-[#3b3b3b]">
-          <div className="h-[400px] w-[510px] bg-[url(/images/hero.png)] bg-cover bg-center" />
+        <div className="flex flex-col overflow-hidden rounded-[20px] bg-[#3b3b3b] md:h-[330px] md:w-[330px] lg:h-[510px] lg:w-[510px]">
+          <div className="bg-[url(/images/hero.png)] bg-cover bg-center md:h-[221px] md:w-[330px] lg:h-[400px] lg:w-[510px]" />
           <div className="my-auto flex flex-col justify-center gap-[10px] bg-inherit px-[20px] py-[22px]">
             <div className="bg-inherit text-[22px] font-semibold leading-[1.4]">
               Space Walking
