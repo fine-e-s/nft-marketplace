0bc8c9846429a3eb7352c1ecec7b084b143c148c
fine-e-s <fineevgeny@gmail.com>
2023-03-14 21:55:56 +0400

    Make marketplace adaptive

--- src/pages/marketplace/index.js
+++ src/pages/marketplace/index.js
@@ -1,7 +1,7 @@
 export default function Marketplace() {
   return (
     <div className="flex w-full justify-center bg-[#3b3b3b]">
-      <div className="grid w-[1050px] grid-cols-3 gap-[30px] bg-inherit pt-[60px] pb-[80px]">
+      <div className="grid w-max gap-[30px] bg-inherit pt-[60px] pb-[80px] sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {data.map((dataElement, i) => (
           <div
             className="flex h-[470px] w-[330px] flex-col overflow-hidden rounded-[20px] border-2 border-transparent bg-[#2b2b2b] bg-clip-content transition duration-300 ease-in-out hover:scale-105 hover:border-[#2b2b2b] hover:bg-[#3b3b3b]"
