ca1e4c5ab8d0e0d4445529ce3978051faa265ad6
fine-e-s <fineevgeny@gmail.com>
2023-03-14 21:50:20 +0400

    Make Categories section adaptive

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,18 +1,18 @@
 export default function Categories() {
   return (
-    <div className="my-0 flex flex-col gap-[60px] py-[80px]">
-      <div className="text-[38px] font-semibold leading-[1.2]">
+    <div className="my-[40px] flex flex-col gap-[60px]">
+      <div className="text-[28px] font-semibold leading-[1.2] lg:text-[38px]">
         Browse Categories
       </div>
       <div className="grid grid-cols-4 gap-[30px]">
         {categories.map((category, i) => (
           <div
-            className="flex h-[316px] w-[240px] flex-col overflow-hidden rounded-[20px] border-2 border-transparent bg-[#3b3b3b] bg-clip-content transition duration-300 ease-in-out hover:scale-105 hover:border-[#3b3b3b] hover:bg-[#2b2b2b]"
+            className="flex h-[210px] w-[150px] cursor-pointer flex-col overflow-hidden rounded-[20px] border-2 border-transparent bg-[#3b3b3b] bg-clip-content transition duration-300 ease-in-out hover:scale-105 hover:border-[#3b3b3b] hover:bg-[#2b2b2b] lg:h-[320px] lg:w-[240px]"
             key={category.name + i}
           >
             <div className="flex items-center justify-center overflow-hidden">
               <div
-                className="h-[240px] w-[240px] scale-[1.1] bg-inherit bg-cover bg-center blur-[8px] contrast-[.90]"
+                className="h-[142px] w-[150px] scale-[1.1] bg-inherit bg-cover bg-center blur-[8px] contrast-[.90] lg:h-[240px] lg:w-[240px]"
                 style={{ backgroundImage: `url(images/${category.img})` }}
               />
               <div className="absolute bg-transparent">
@@ -22,7 +22,7 @@ export default function Categories() {
                 />
               </div>
             </div>
-            <div className="bg-inherit pl-[30px] pt-[20px] text-[22px] font-semibold leading-[1.4]">
+            <div className="bg-inherit pl-[16px] pt-[22px] text-[14px] font-semibold leading-[1.4] lg:pl-[30px] lg:text-[22px]">
               {category.name}
             </div>
           </div>
