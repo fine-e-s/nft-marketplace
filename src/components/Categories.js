9ddb34cbdacf8ac3df0419a0560d17ab995b8650
fine-e-s <fineevgeny@gmail.com>
2023-03-14 23:35:09 +0400

    Make Categories adaptive to small displays

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,7 +4,7 @@ export default function Categories() {
       <div className="text-[28px] font-semibold leading-[1.2] lg:text-[38px]">
         Browse Categories
       </div>
-      <div className="grid grid-cols-4 gap-[30px]">
+      <div className="grid grid-cols-4 gap-[30px] max-md:grid-cols-2">
         {categories.map((category, i) => (
           <div
             className="flex h-[210px] w-[150px] cursor-pointer flex-col overflow-hidden rounded-[20px] border-2 border-transparent bg-[#3b3b3b] bg-clip-content transition duration-300 ease-in-out hover:scale-105 hover:border-[#3b3b3b] hover:bg-[#2b2b2b] lg:h-[320px] lg:w-[240px]"
