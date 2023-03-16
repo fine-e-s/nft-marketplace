2ac996905464c4b9a780b2ed41127b469197bf1d
fine-e-s <fineevgeny@gmail.com>
2023-03-17 00:54:51 +0400

    Edit animation

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -7,8 +7,8 @@ export default function Registration() {
     <>
       <div
         onClick={regToggle}
-        className={`fixed inset-0 z-20 flex h-full w-full items-center justify-center bg-black bg-opacity-70
-        ${isRegOpened ? "" : "hidden"}`}
+        className={`fixed inset-0 z-20 flex h-full w-full items-center justify-center bg-black bg-opacity-70 transition duration-1000
+        ${isRegOpened ? "" : "pointer-events-none opacity-0"}`}
       ></div>
       <div
         className={`fixed top-1/2 left-1/2 z-30 h-96 w-96 -translate-x-1/2 -translate-y-1/2 rounded-[20px] bg-lighter transition-all duration-300 ${
