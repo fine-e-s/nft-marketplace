170564c06f1511d8de7fa646e5d7380cb502c1f0
fine-e-s <fineevgeny@gmail.com>
2023-03-30 23:45:36 +0400

    Move Provider to _app

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,15 @@
 import Header from "./Header";
 import Registration from "./Registration";
 import Transition from "./Transition";
-import Provider from "./Provider";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Provider>
-        <Registration />
-        <Header />
-        <Transition>
-          <div className="flex flex-col items-center">{children}</div>
-        </Transition>
-      </Provider>
+      <Registration />
+      <Header />
+      <Transition>
+        <div className="flex flex-col items-center">{children}</div>
+      </Transition>
     </>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import Provider from "@/components/Provider";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
