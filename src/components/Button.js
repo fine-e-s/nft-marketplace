b2b6cb71d36cb1c7e853dea2b933eb185a66b4e9
fine-e-s <fineevgeny@gmail.com>
2023-03-30 22:24:46 +0400

    Minor change

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,7 +10,7 @@ export default function Button({
   return (
     <div
       onClick={onClick}
-      className={`mx-[10px] cursor-pointer py-3 lg:mx-5 ${
+      className={`mx-[10px] cursor-pointer bg-inherit py-3 lg:mx-5 ${
         cta
           ? "!mx-0 flex items-center gap-3 rounded-[20px] bg-purple-500 px-7 py-5 font-semibold"
           : ""
