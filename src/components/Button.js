8914426cd885a05e8ddd1eb72435e07f532f0c9f
fine-e-s <fineevgeny@gmail.com>
2023-03-30 18:31:15 +0400

    Fix font

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,7 +12,7 @@ export default function Button({
       onClick={onClick}
       className={`mx-[10px] cursor-pointer py-3 lg:mx-5 ${
         cta
-          ? "!mx-0 flex items-center gap-3 rounded-[20px] bg-purple-500 px-7 py-5"
+          ? "!mx-0 flex items-center gap-3 rounded-[20px] bg-purple-500 px-7 py-5 font-semibold"
           : ""
       } ${
         large
