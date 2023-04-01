10e7e8ebac507dba0d9a24840f862ee8c2f0bcd3
fine-e-s <fineevgeny@gmail.com>
2023-04-01 22:26:44 +0400

    Add className prop; Center text

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,11 +6,13 @@ export default function Button({
   hoverUnderline,
   children,
   onClick,
+  className,
 }) {
   return (
     <div
       onClick={onClick}
-      className={`mx-[10px] cursor-pointer bg-inherit py-3 lg:mx-5 ${
+      className={`mx-[10px] cursor-pointer justify-center bg-inherit py-3 lg:mx-5 ${className} 
+      ${
         cta
           ? "!mx-0 flex items-center gap-3 rounded-[20px] bg-purple-500 px-7 py-5 font-semibold"
           : ""
