c82209b666d1b88ad7e593c84081810f4b526620
fine-e-s <fineevgeny@gmail.com>
2023-04-02 15:46:16 +0400

    Remove userContextProvider

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,15 +1,12 @@
 import { RegContextProvider } from "@/hooks/useReg";
 import { MenuContextProvider } from "@/hooks/useMenu";
-import { UserContextProvider } from "@/hooks/useUser";
 
 export default function Provider({ children }) {
   return (
     <>
-      <UserContextProvider>
-        <RegContextProvider>
-          <MenuContextProvider>{children}</MenuContextProvider>
-        </RegContextProvider>
-      </UserContextProvider>
+      <RegContextProvider>
+        <MenuContextProvider>{children}</MenuContextProvider>
+      </RegContextProvider>
     </>
   );
 }
