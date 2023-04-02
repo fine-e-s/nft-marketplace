c353db4cb2f0a46a39dab41a79a117f76b185256
fine-e-s <fineevgeny@gmail.com>
2023-04-02 20:49:57 +0400

    Load UI after fetching auth state

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,25 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import Header from "./Header";
 import Registration from "./Registration";
 import Transition from "./Transition";
+import { auth } from "@/firebase/firebaseApp";
 
 export default function Layout({ children }) {
+  const [user, loading] = useAuthState(auth);
+
+  console.log(loading);
+
   return (
     <>
-      <Registration />
-      <Header />
-      <Transition>
-        <div className="flex flex-col items-center">{children}</div>
-      </Transition>
+      {!loading && (
+        <>
+          <Registration />
+          <Header />
+          <Transition>
+            <div className="flex flex-col items-center">{children}</div>
+          </Transition>
+        </>
+      )}
     </>
   );
 }
