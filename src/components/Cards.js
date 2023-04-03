8835350096b163fcd498f602f116f90f14a8cd15
fine-e-s <fineevgeny@gmail.com>
2023-04-04 01:21:21 +0400

    Fix trying to read null from data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -40,60 +40,59 @@ export default function Cards() {
         <AnimatePresence mode="wait">
           {cardsLoading ? (
             <Loading />
-          ) : data.length > 0 ? (
+          ) : data && data.length > 0 ? (
             <div className="grid w-max gap-[30px] bg-inherit pt-[60px] pb-[80px] sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-              {data &&
-                data.map((dataElement, i) => (
-                  <motion.div
-                    className="flex h-[470px] w-[330px] flex-col overflow-hidden rounded-[20px] border-2 border-transparent bg-[#2b2b2b] bg-clip-content transition duration-300 ease-in-out hover:scale-105 hover:border-[#2b2b2b] hover:bg-[#3b3b3b]"
-                    key={dataElement.name + i}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 2 }}
-                    transition={{ duration: 0.2 }}
-                  >
-                    <div
-                      className="h-[295px] w-[330px] bg-cover bg-center"
-                      style={{ backgroundImage: `url(${dataElement.url})` }}
-                    ></div>
-                    <div className="mx-0 my-auto flex flex-col gap-[25px] bg-inherit">
-                      <div className="flex flex-col gap-[6px] bg-inherit px-[30px]">
-                        <div className="bg-inherit text-[22px] font-semibold leading-[1.4]">
-                          {dataElement.name}
-                        </div>
-                        <div className="flex gap-[12px] bg-inherit">
-                          <div
-                            className="h-[24px] w-[24px] rounded-full bg-inherit bg-cover bg-center"
-                            style={{
-                              backgroundImage: `url(images/avatar-1.png)`,
-                            }}
-                          ></div>
-                          <div className="bg-inherit font-mono text-[16px] font-extralight leading-[1.4]">
-                            {dataElement.creator}
-                          </div>
-                        </div>
+              {data.map((dataElement, i) => (
+                <motion.div
+                  className="flex h-[470px] w-[330px] flex-col overflow-hidden rounded-[20px] border-2 border-transparent bg-[#2b2b2b] bg-clip-content transition duration-300 ease-in-out hover:scale-105 hover:border-[#2b2b2b] hover:bg-[#3b3b3b]"
+                  key={dataElement.name + i}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 2 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <div
+                    className="h-[295px] w-[330px] bg-cover bg-center"
+                    style={{ backgroundImage: `url(${dataElement.url})` }}
+                  ></div>
+                  <div className="mx-0 my-auto flex flex-col gap-[25px] bg-inherit">
+                    <div className="flex flex-col gap-[6px] bg-inherit px-[30px]">
+                      <div className="bg-inherit text-[22px] font-semibold leading-[1.4]">
+                        {dataElement.name}
                       </div>
-                      <div className="flex justify-center bg-inherit">
-                        <div className="flex w-[135px] flex-col items-start gap-[8px] bg-inherit">
-                          <div className="bg-inherit font-mono text-[12px] leading-[1.1] text-[#858584]">
-                            Price
-                          </div>
-                          <div className="bg-inherit font-mono text-[16px] leading-[1.4]">
-                            {dataElement.price} ETH
-                          </div>
-                        </div>
-                        <div className="flex w-[135px] flex-col items-end gap-[8px] bg-inherit">
-                          <div className="flex w-[135px] flex-col items-end gap-[8px] bg-inherit font-mono text-[12px] leading-[1.1] text-[#858584]">
-                            Highest Bid
-                          </div>
-                          <div className="bg-inherit font-mono text-[16px] leading-[1.4]">
-                            {dataElement.highestBid} wETH
-                          </div>
+                      <div className="flex gap-[12px] bg-inherit">
+                        <div
+                          className="h-[24px] w-[24px] rounded-full bg-inherit bg-cover bg-center"
+                          style={{
+                            backgroundImage: `url(images/avatar-1.png)`,
+                          }}
+                        ></div>
+                        <div className="bg-inherit font-mono text-[16px] font-extralight leading-[1.4]">
+                          {dataElement.creator}
                         </div>
                       </div>
                     </div>
-                  </motion.div>
-                ))}
+                    <div className="flex justify-center bg-inherit">
+                      <div className="flex w-[135px] flex-col items-start gap-[8px] bg-inherit">
+                        <div className="bg-inherit font-mono text-[12px] leading-[1.1] text-[#858584]">
+                          Price
+                        </div>
+                        <div className="bg-inherit font-mono text-[16px] leading-[1.4]">
+                          {dataElement.price} ETH
+                        </div>
+                      </div>
+                      <div className="flex w-[135px] flex-col items-end gap-[8px] bg-inherit">
+                        <div className="flex w-[135px] flex-col items-end gap-[8px] bg-inherit font-mono text-[12px] leading-[1.1] text-[#858584]">
+                          Highest Bid
+                        </div>
+                        <div className="bg-inherit font-mono text-[16px] leading-[1.4]">
+                          {dataElement.highestBid} wETH
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </motion.div>
+              ))}
             </div>
           ) : (
             <div className="flex h-80 items-center bg-inherit">
