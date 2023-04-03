fdbd612ad0c712124812c412c1637e0cb9b6e378
fine-e-s <fineevgeny@gmail.com>
2023-04-03 15:13:22 +0400

    Reset category if exited marketplace

--- src/hooks/useCategory.js
+++ src/hooks/useCategory.js
@@ -1,13 +1,19 @@
-const { createContext, useState, useContext } = require("react");
+import { createContext, useState, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const CategoryContext = createContext(null);
 
 export function CategoryContextProvider({ children }) {
   const [category, setCategory] = useState(null);
+  const router = useRouter();
 
   function newCategory(category) {
     category ? setCategory(category) : setCategory(null);
   }
+
+  useEffect(() => {
+    newCategory(null);
+  }, [router.pathname]);
 
   return (
     <CategoryContext.Provider value={{ category, newCategory }}>
