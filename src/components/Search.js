45dd0bec9713d73b93a51a487a8bbe0eea8e46e4
fine-e-s <fineevgeny@gmail.com>
2023-04-05 21:02:48 +0400

    Add search placeholder text

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,8 +10,9 @@ export default function Search() {
         <div className="flex h-12 items-center rounded-[20px] border-2 border-lighter bg-inherit">
           <div className="mx-3 flex w-full bg-inherit">
             <input
-              className="mx-2 w-full bg-inherit"
+              className="mx-2 w-full bg-inherit placeholder:text-lighter"
               type="search"
+              placeholder="Search..."
               onChange={(e) => setPrompt(e.target.value)}
             />
             <img src="icons/search.svg" className="bg-inherit" />
