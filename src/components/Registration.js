24fdbdc34bacc0bdcfcb3a338746381b8de2e300
fine-e-s <fineevgeny@gmail.com>
2023-04-05 23:50:27 +0400

    Add Sign In/Sign Up switch

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { useReg } from "@/hooks/useReg";
 import Button from "./Button";
@@ -11,10 +11,20 @@ import { auth } from "@/firebase/firebaseApp";
 export default function Registration() {
   const { isRegOpened, regToggle } = useReg();
 
+  useEffect(() => {
+    setTimeout(() => {
+      setSignUp(false);
+    }, 300);
+  }, [isRegOpened]);
+
+  const [isSignUp, setSignUp] = useState(false);
+
   return (
     <>
       <div
-        onClick={regToggle}
+        onClick={() => {
+          regToggle();
+        }}
         className={`fixed z-20 flex h-full w-full items-center justify-center bg-black bg-opacity-60 backdrop-blur-sm transition duration-1000
         ${isRegOpened ? "" : "pointer-events-none opacity-0"}`}
       ></div>
@@ -25,15 +35,13 @@ export default function Registration() {
             : "pointer-events-none scale-150 opacity-0"
         }`}
       >
-        <LogInForm />
-        <div className=" m-4 bg-lighter max-md:h-px sm:md:w-px" />
-        <SignUpForm />
+        {isSignUp ? <SignUpForm /> : <LogInForm setSignUp={setSignUp} />}
       </div>
     </>
   );
 }
 
-function LogInForm() {
+function LogInForm({ setSignUp }) {
   const { regToggle } = useReg();
 
   const [email, setEmail] = useState("");
@@ -54,13 +62,13 @@ function LogInForm() {
 
   return (
     <>
-      <div className="flex h-full w-1/2 flex-col justify-between bg-transparent pt-3">
-        <form className="flex flex-col bg-inherit">
-          <div className="mx-3 grid grid-flow-col items-center bg-inherit">
+      <div className="m-3 flex h-full flex-col justify-between bg-transparent pt-3">
+        <form className="flex flex-col gap-5 bg-inherit">
+          <div className="grid grid-flow-col items-center bg-inherit">
             <label htmlFor="1" className="m-3 w-36 bg-inherit">
               Email:
             </label>
-            <div className="m-3 flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
+            <div className="flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
               <input
                 onChange={(e) => setEmail(e.target.value)}
                 type="email"
@@ -71,11 +79,11 @@ function LogInForm() {
             </div>
           </div>
 
-          <div className="mx-3 grid grid-flow-col items-center bg-inherit">
+          <div className="grid grid-flow-col items-center bg-inherit">
             <label htmlFor="2" className="m-3 w-36 bg-inherit">
               Password:
             </label>
-            <div className="m-3 flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
+            <div className="flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
               <input
                 onChange={(e) => setPassword(e.target.value)}
                 type="password"
@@ -86,7 +94,7 @@ function LogInForm() {
             </div>
           </div>
         </form>
-        <div className="mx-5 mb-6 flex justify-center gap-4">
+        <div className="mx-2 mt-5 flex justify-center">
           <Button
             cta
             hoverScale
@@ -98,6 +106,14 @@ function LogInForm() {
           >
             Log In
           </Button>
+        </div>
+        <div
+          className="mt-2 translate-y-1 cursor-pointer text-center text-sm text-light underline"
+          onClick={() => {
+            setSignUp(true);
+          }}
+        >
+          Don't have an account?
         </div>
       </div>
     </>
@@ -134,13 +150,13 @@ function SignUpForm() {
 
   return (
     <>
-      <div className="flex h-full w-1/2 flex-col justify-between bg-transparent pt-3">
-        <form className="flex flex-col bg-inherit">
-          <div className="mx-3 grid grid-flow-col items-center bg-inherit">
+      <div className="m-3 flex h-full flex-col justify-between bg-transparent pt-3">
+        <form className="flex flex-col gap-5 bg-inherit">
+          <div className="grid grid-flow-col items-center bg-inherit">
             <label htmlFor="1" className="m-3 w-36 bg-inherit">
               Email:
             </label>
-            <div className="m-3 flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
+            <div className="flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
               <input
                 onChange={(e) => setEmail(e.target.value)}
                 type="email"
@@ -151,11 +167,11 @@ function SignUpForm() {
             </div>
           </div>
 
-          <div className="mx-3 grid grid-flow-col items-center bg-inherit">
+          <div className="grid grid-flow-col items-center bg-inherit">
             <label htmlFor="2" className="m-3 w-36 bg-inherit">
               Password:
             </label>
-            <div className="m-3 flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
+            <div className="flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
               <input
                 onChange={(e) => setPassword(e.target.value)}
                 type="password"
@@ -166,11 +182,11 @@ function SignUpForm() {
             </div>
           </div>
 
-          <div className="mx-3 grid grid-flow-col items-center bg-inherit">
+          <div className="grid grid-flow-col items-center bg-inherit">
             <label htmlFor="3" className="m-3 w-36 bg-inherit">
               Repeat password:
             </label>
-            <div className="m-3 flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
+            <div className="flex h-10 w-40 justify-self-end rounded-2xl border-2 border-lighter">
               <input
                 onChange={(e) => setRepeatPassword(e.target.value)}
                 type="password"
@@ -181,7 +197,7 @@ function SignUpForm() {
             </div>
           </div>
         </form>
-        <div className="mx-5 mb-6 flex justify-center gap-4">
+        <div className="mx-2 mt-5 mb-1 flex justify-center gap-4">
           <Button
             cta
             hoverScale
