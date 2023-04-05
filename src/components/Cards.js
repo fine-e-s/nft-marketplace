f577185427b5cd61b997f879fd5712c216860830
fine-e-s <fineevgeny@gmail.com>
2023-04-06 01:23:12 +0400

    Fix exit animation

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -51,7 +51,7 @@ export default function Cards() {
                   key={dataElement.name + i}
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
-                  exit={{ opacity: 2 }}
+                  exit={{ opacity: 0 }}
                   transition={{ duration: 0.2 }}
                 >
                   <div
