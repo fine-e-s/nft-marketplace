ca97f78b56fc858187889071cf83b3b57d402e81
fine-e-s <fineevgeny@gmail.com>
2023-04-10 22:49:24 +0400

    Replace css animation with gsap

--- src/hooks/useReg.js
+++ src/hooks/useReg.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { gsap } from "gsap";
 
 const RegContext = createContext();
 
@@ -6,7 +7,25 @@ export function RegContextProvider({ children }) {
   const [isRegOpened, setRegOpened] = useState(false);
 
   function regToggle() {
+    gsapRegToggle(isRegOpened);
     setRegOpened(!isRegOpened);
+  }
+
+  function gsapRegToggle(isRegOpened) {
+    if (isRegOpened) {
+      gsap.fromTo(
+        "[data-regform]",
+        { scale: 1, autoAlpha: 1 },
+        { scale: 1.5, autoAlpha: 0, duration: 0.3 }
+      );
+    } else {
+      gsap.set("[data-loginform]", { x: 0, autoAlpha: 1 });
+      gsap.fromTo(
+        "[data-regform]",
+        { scale: 1, autoAlpha: 0 },
+        { scale: 1, autoAlpha: 1, duration: 0.3 }
+      );
+    }
   }
 
   return (
