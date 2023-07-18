4854a6c0e8d809b845c819c29cce2af7bb1ee49d
fine-e-s <fineevgeny@gmail.com>
2023-07-18 20:26:38 +0400

    Implement loader anim with gsap

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { gsap } from "gsap";
 
 export default function Loading() {
+  let tl = gsap.timeline({ repeat: -1 });
+  tl.to("#square", {
+    rotation: 90,
+    duration: 0.5,
+    scale: 1.1,
+    ease: "none",
+    stagger: 0.2,
+  });
+
   return (
     <div className="flex h-96 w-4/5 items-center justify-between bg-inherit">
       <Squares />
@@ -14,8 +24,11 @@ function Squares() {
   for (let i = 0; i < 3; i++) {
     out.push(
       <React.Fragment key={i}>
-        <div className="flex h-14 w-14 items-center justify-center bg-purple-500">
-          <div className="h-4 w-4 bg-lighter" />
+        <div
+          className="flex h-14 w-14 items-center justify-center rounded-md bg-purple-500"
+          id="square"
+        >
+          <div className="h-5 w-5 rounded-md bg-lighter" />
         </div>
       </React.Fragment>
     );
