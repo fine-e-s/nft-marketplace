e47c01c35d6d54b58fe4bf5491ab8f6b93c757f1
fine-e-s <fineevgeny@gmail.com>
2023-07-18 19:51:05 +0400

    Remove framer-motion leftovers

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -15,22 +15,7 @@ function Squares() {
     out.push(
       <React.Fragment key={i}>
         <div className="flex h-14 w-14 items-center justify-center bg-purple-500">
-          <div
-            className="h-4 w-4 bg-lighter"
-            animate={{
-              scale: [1, 2, 2, 1, 1],
-              rotate: [0, 0, 180, 225, 360],
-              borderRadius: ["30%", "30%", "50%", "50%", "30%"],
-            }}
-            transition={{
-              delay: `${0.7 + 0.5 * i}`,
-              duration: 2,
-              ease: "easeInOut",
-              times: [0, 0.2, 0.5, 0.8, 1],
-              repeat: Infinity,
-              repeatDelay: 1,
-            }}
-          />
+          <div className="h-4 w-4 bg-lighter" />
         </div>
       </React.Fragment>
     );
