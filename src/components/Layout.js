ccc0ccb5ba28b3809565c5b12a4dc68e2a926763
fine-e-s <fineevgeny@gmail.com>
2023-07-19 02:55:27 +0400

    Implement page transitions in gsap

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,33 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import Header from "./Header";
 import Registration from "./Registration";
-import Transition from "./Transition";
 import { auth } from "@/firebase/firebaseApp";
+import { gsap } from "gsap";
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
+  const [currentChildren, setChildren] = useState(children);
   const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+  const el = useRef();
+
+  useEffect(() => {
+    if (children !== currentChildren) {
+      let tl = gsap.timeline();
+
+      tl.to(el.current, {
+        opacity: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+        onComplete: () => setChildren(children),
+      }).to(el.current, {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      });
+    }
+  }, [router.route]);
 
   return (
     <>
@@ -13,9 +35,9 @@ export default function Layout({ children }) {
         <>
           <Registration />
           <Header />
-          <Transition>
-            <div className="flex flex-col items-center">{children}</div>
-          </Transition>
+          <div ref={el} className="flex flex-col items-center">
+            {currentChildren}
+          </div>
         </>
       )}
     </>
