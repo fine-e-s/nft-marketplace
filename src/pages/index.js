ee54b70660a44585d549d126e13d66ad138d62f6
fine-e-s <fineevgeny@gmail.com>
2024-04-27 18:44:56 +0700

    Add fade in animations to sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
+import { gsap } from "gsap/dist/gsap";
 import Hero from "@/components/Hero";
 import Trending from "@/components/Trending";
 import Categories from "@/components/Categories";
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function Home() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    document.querySelectorAll("#section").forEach((section) =>
+      gsap.to(section, {
+        opacity: 1,
+        delay: 0.2,
+        scrollTrigger: {
+          trigger: section,
+          start: "top center",
+        },
+      })
+    );
+  }, []);
   return (
     <>
       <Hero />
